fe783bc8c698da6e1622bdaaae8b0a6aee6ced65
Espi <ethyn.gillies@gmail.com>
2023-09-22 21:19:02 +1200

    Typo in babel config fixed

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,10 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    'module:react-native-dotenv',
-    {
-      moduleName: "@env",
-      path: ".env",
-    },
+    ['module:react-native-dotenv',
+      {
+        "moduleName": "@env",
+        "path": ".env"
+      }],
   ],
 };
