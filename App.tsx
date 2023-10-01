5d5b41adbfc00be84fc3f199c94340d4a9b02c08
iiKurt <38019652+iiKurt@users.noreply.github.com>
2023-10-01 18:34:57 +1300

    Disabling system dark mode
    😢

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Appearance } from 'react-native';
 import { AuthProvider } from './contexts/AuthContext';
 import { DummyListProvider } from './contexts/DummyContext';
 import { NavigationScreen } from './screens/NavigationScreen'
 
 function App(): JSX.Element {
+  useEffect(() => Appearance.setColorScheme('light'),
+  [])
+
   // Dummy list provider context is temporary
   return (
     <DummyListProvider>
